f5dcd834507b0a716d7b4691e687548db91d3282
Vanness Aurelius Gunawan <vaurelius03@student.ciputra.ac.id>
2026-05-29 06:31:29 +0700

    Initial commit: Setup SwiftUI project, XCTest, and Firebase dependencies

---
+++ alp/alp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alp
+//
+//  Created by Vanness Aurelius Gunawan on 29/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alp/alp/alpApp.swift
@@ -0,0 +1,17 @@
+//
+//  alpApp.swift
+//  alp
+//
+//  Created by Vanness Aurelius Gunawan on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct alpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
