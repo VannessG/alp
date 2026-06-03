86217225eb69606c29f0d326cadbdd9db5fd8b3f
Vincent <vincent01@student.ciputra.ac.id>
2026-06-03 14:10:11 -0700

    Menyelesaikan Schedule View

---
+++ alp/alp/Views/Schedule/ScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleView.swift
+//  alp
+//
+//  Created by Vincent on 03/06/26.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
